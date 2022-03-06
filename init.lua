9a1c2df40b9a4958703c13697efdd6be9eebb73c
Erick <edag94@live.com>
2022-03-05 22:11:19 -0500

    fixed visible bug

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nv
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
 end
-visible
+
 vim.cmd [[
   augroup Packer
     autocmd!
